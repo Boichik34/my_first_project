da567a6126de19767efedd49683125948d6a12d7
Артём Бойко <timboiko38@mail.ru>
2023-08-13 15:13:12 +0300

    feat: Add main

---
+++ main.py
@@ -0,0 +1,12 @@
+import controller
+
+
+def main():
+    print("------------------------------")
+    print("Добро пожаловать в Ваш каталог книг!")
+
+    controller.print_commands()
+
+
+if __name__ == '__main__':
+    main()
